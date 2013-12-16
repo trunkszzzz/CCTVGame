49e1e43c4f577668c458afc63c42d10834ae3f15
pwrd <pwrd@jkoons-lt.perfectworld.local>
2013-12-15 17:18:12 -0800

    second11

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,6 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
+  console.log(request.params);
   response.success("Hello world!");
 });
