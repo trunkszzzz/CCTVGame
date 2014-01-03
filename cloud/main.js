abac46f729fbc92addea29a22cfda53156fc2a6c
pwrd <pwrd@jkoons-lt.perfectworld.local>
2014-01-02 22:06:04 -0800

    submit66

--- cloud/main.js
+++ cloud/main.js
@@ -115,11 +115,18 @@ AV.Cloud.define("getRanking", function(request, response){
 				var userData = results[index];
 				if (userData.get("username") == theUser.get("username")){
 					var ret = {};
-					for (var index1 = 0; index1 < 11; index1++){
-						var i = index - 11;
-						if (i < 0)
+					for (var index1 = 0; index1 < 5; index1++){
+						var i = index + index1;
+						if (i < 0 || i >= results.length)
 							continue;
-						var ud = results[index1+i];
+						var ud = results[i];
+						ret[ud.get("username")] = ud.get("TotalScore");
+					}
+					for (var index1 = 1; index1 < 5; index1++){
+						var i = index - index1;
+						if (i < 0 || i >= results.length)
+							continue;
+						var ud = results[i];
 						ret[ud.get("username")] = ud.get("TotalScore");
 					}
 					response.success(ret);
