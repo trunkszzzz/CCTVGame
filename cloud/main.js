cc2a647e462809d0033d31e3a567c0429e07c9e7
pwrd <pwrd@pwrdtekiMacBook-Air.local>
2014-01-19 18:49:52 +0800

    submit66

--- cloud/main.js
+++ cloud/main.js
@@ -362,9 +362,9 @@ AV.Cloud.define("testAdjustTime", function(request, response){
 						var startTime = brand.get("StartTime");
 						var endTime = brand.get("EndTime");
 						startTime.setHours(nowTime.getHours());
-						startTime.setMinutes(nowTime.getMinutes()+1);
+						startTime.setMinutes(nowTime.getMinutes()+3);
 						endTime.setHours(nowTime.getHours());
-						endTime.setMinutes(nowTime.getMinutes()+1);
+						endTime.setMinutes(nowTime.getMinutes()+3);
 						brand.set("StartTime", startTime);
 						brand.set("EndTime", endTime);
 						brand.save();
